6d0b9f835ca52d97c2b2df35fdea483de99b307e
Ukchuvrus <stdimitriev@mail.ru>
2015-09-14 20:31:02 +0300

    1) Самое главное. Number не наследует CalcMethods. Number нужен для того, чтобы описать числа. CalcMethods для того, чтобы создать объект, который будет уметь складывать числа.
    2) Убрал ненужное поле из RealNumber.
    
    3) Урал main-ы из классов моделей чисел. Они там не нужны, нужно описать отдельный класс, который будет демонстрировать функциональность всех классов. (Этот пункт, был в принципе не обязателен, так как проектик модельный. Но все равно плохо смешивать логику использования класса с описанием класса)
    
    4) Кое-где проблемы с CamelCase и форматированием кода

--- src/RealNumber.java
+++ src/RealNumber.java
@@ -1,12 +1,14 @@
 /**
  * Created by 111 on 14.09.2015.
  */
-public class RealNumber extends Number{
-    int Rx, Iy;
+public class RealNumber extends Number {
+    // зачем iy тут?
+    // кстати, не забывай про CamelCase.
+    private Double rx;
 
     @Override
     public void addition(Number a, Number b) {
-     int add = a.getX(a) + b.getX(b);
+        int add = a.getX(a) + b.getX(b);
         System.out.println("The result of addition is " + add);
 
     }
@@ -19,43 +21,34 @@ public class RealNumber extends Number{
 
     @Override
     public void multiplication(Number a, Number b) {
-        int add = a.getX(a)*b.getX(b);
+        int add = a.getX(a) * b.getX(b);
         System.out.println("The result of substruction is " + add);
     }
 
     @Override
     public void division(Number a, Number b) {
-        int add = a.getX(a)*b.getX(b);
+        int add = a.getX(a) * b.getX(b);
         System.out.println("The result of division is " + add);
     }
 
     @Override
     public int getX(Number a) {
-        return Rx;
-    }
-
-
-    @Override
-    public int getIy(Number a) {
-        return 0;
+        return rx;
     }
 
     @Override
     public void setX(int a) {
-      Rx = a;
-    }
-    @Override
-    public void setIy(int a){
-        Iy = 0;
+        rx = a;
     }
 
+    //  main предлагаю вынести в отдельный класс, в котором можно будет посмотреть весь функционал для всех классов.
 
-    public static void main(String argv[]){
-       Number a = new RealNumber();
-       Number b = new RealNumber();
-        a.setX(14);
-        b.setX(88);
-        a.addition(a,b);
-
-    }
+//    public static void main(String argv[]) {
+//        Number a = new RealNumber();
+//        Number b = new RealNumber();
+//        a.setX(14);
+//        b.setX(88);
+//        a.addition(a, b);
+//
+//    }
 }
---
+++ src/СalcMethods.java
@@ -0,0 +1,14 @@
+/**
+ * Created by 111 on 14.09.2015.
+ */
+public interface ÑalcMethods {
+
+    public Number addition(Number a, Number b);
+
+    public Number subsctruction(Number a, Number b);
+
+    public Number multiplication(Number a, Number b);
+
+    public Number division(Number a, Number b);
+
+}
